e1bcb0d900e293bf9929abafec41ed0b423ec8d8
zsyzsy <552068363@qq.com>
2017-03-25 19:56:36 +0800

    zhaoping

--- js/index.js
+++ js/index.js
@@ -18,34 +18,39 @@ $(function(){
 		}
 	});
 
-
-
-
-
-
-//if(isWeiXin()){
-//		alert("在微信中打开");
-//}
-//function isWeiXin() {
-//	var ua = window.navigator.userAgent.toLowerCase(); //获取判断用的对象
-//	if (ua.match(/MicroMessenger/i) == 'micromessenger') {
-//		return true;
-//	} else {
-//		return false;
-//	}
-//}
-
-
 	var bodyWidth = window.screen.width;
 	var bodyHeight = window.screen.height;
 	//alert(bodyWidth);
 	//alert(bodyHeight);
-	if(bodyWidth==320 && bodyHeight==480){  //iphne5
+	if(bodyWidth==320 && bodyHeight==480){  //iphne4
+		$(".page1_3").css({"top":"31%"});
 		$(".page1_1").css("width","75%");
 		$(".page1_5").css("height","16%");
+		$(".page2_2_1").css({"marginTop":"16%","marginLeft":"27%"});
+		$(".page2_3_1").css({"marginTop":"39%"});
+		$(".page2_4_1").css({"marginTop":"18%"});
+		$(".page2_6_1").css({"marginTop": "1%"});
+		$(".page2_6_2").css({"top": "-2%"});
+		$(".page2_6_3").css({"top": "9%"});
 		$(".page3_4").css({backgroundPosition:"0% 0%",backgroundSize:"103%"});
 		$(".page3_text_text").css("lineHeight","12px");
-		$(".page3_text").css("bottom","25%");
+		$(".page3_text").css("bottom","27%");
+		$(".page4_1").css({"width":"65%"});
+		$(".page4_zhiwei").css({"margin":"-3% auto 0;"});
+		$(".page5_7").css({"bottom":"32%"});
+		$("ul.page6_fuli").css({"lineHeight":"20px"});
+		$(".page6_1").css({"backgroundSize":"84%"});
+		$(".page6_dingwei").css({"height":"24%"});
+		$(".page6_naozhong,.page6_liwu").css({"height":"18%"});
+		$(".page6_jinqian").css({"height":"20%"});
+		$(".page7_2").css({"top":"53%"});
+		$(".page7_yuan").css({"top":"66%"});
+		$(".page7_tel").css({"top":"75%","left": "20%"});
+		$(".page7_text").css({"top":"77%"});
+	}
+	if(bodyWidth==320 && bodyHeight==568){
+		$(".page_67").css({"marginTop":"10%"});
+		$(".page3_text_text").css({"lineHeight":"15px"});
 	}
 	if(bodyWidth==360 && bodyHeight==640){  //360 640
 		$(".page1_1").css("width","80%");
@@ -85,98 +90,98 @@ $(function(){
 	if(bodyWidth==375 && bodyHeight==667){  //iphne6
 		$(".page1_5 ").css({"height":"15%"});
 		$(".page1_1 ").css({"width":"82%"});
-		$(".page2_2_1").css({"marginTop":"17%","marginLeft":"26%"});
-		$(".page2_3_1").css({"marginTop":"40%"});
+		$(".page2_2_1").css({"marginTop":"18%","marginLeft":"27%"});
+		$(".page2_3_1").css({"marginTop":"44%"});
 		$(".page2_4_1").css({"marginTop":"19%"});
-		$(".page2_6_1").css({"marginTop":"-15%"});
+		$(".page2_6_1").css({"marginTop":"-16%"});
 		$(".page3_1 ").css({"height":"52%"});
-		$(".page3_4 ").css({"height":"100%","backgroundSize":"100%"});
-		$(".page3_3 ").css({"height":"45%"});
+		$(".page3_4 ").css({"height":"100%","backgroundSize":"107%"});
+		$(".page3_3 ").css({"height":"45%","backgroundSize":"114%"});
 		$(".page3_text ").css({"bottom":"34%"});
-		$(".page3_text_text").css({"fontSize":"14px","lineHeight":"16px"});
+		$(".page3_text_text").css({"fontSize":"14px","lineHeight":"21px"});
 		$(".page4_3 ").css({"height":"45%"});
-		$(".page4_zhiwei ").css({"margin":"4% auto 0"});
+		$(".page4_zhiwei ").css({"margin":"7% auto 0"});
 		$(".page4_renyuan").css({"fontSize":"16px","left":"8%"});
-		$(".page4_jieshao").css({"margin":"5% 0 0 3%","lineHeight":"16px","marginTop":"0"});
-		$(".page5_4").css({"top":"25%"});
-		$(".page5_7").css({"fontSize":"16px","bottom":"24%"});
-		$(".page5_text").css({"fontSize":"14px","width":"93%","lineHeight":"14px","bottom":"0%"});
+		$(".page4_jieshao").css({"margin":"0 0 0 3%","lineHeight":"17px","fontSize":"13px"});
+		$(".page5_4").css({"top":"22%"});
+		$(".page5_5").css({"width":"100%","left":"1%","bottom":"9%"});
+		$(".page5_6").css({"width":"99%","height":"39%","backgroundSize":"131%"});
+		$(".page5_7").css({"fontSize":"22px","bottom":"29%"});
+		$(".page5_text").css({"fontSize":"14px","width":"93%","lineHeight":"26px","bottom":"0%"});
 		$(".page6_1").css({"height":"100%","backgroundSize":"85%"});
 		$(".page6_dingwei").css({"height":"30%","left":"26%"});
 		$(".page6_naozhong").css("height","26%");
 		$(".page6_liwu").css("height","26%");
 		$(".page6_jinqian").css("height","26%");
-		$(".page7_2").css("top","48%");
-		$(".page7_yuan").css({"top":"65%","left":"28%"});
+		$(".page7_2").css("top","45%");
+		$(".page7_yuan").css({"top":"60%","left":"28%"});
 		$(".page7_bottom").css("height","8%");
-		$(".page7_tel").css("top","75%");
+		$(".page7_tel").css("top","70%");
 		$(".page7_tel_tel").css("left","-8%");
 		$(".page7_num").css({"fontSize":"20px","left":"10%"});
 		$(".page7_text").css({"fontSize":"14px","top":"77%","lineHeight":"24px"});
 	}
 	if(bodyWidth==414 && bodyHeight==736){  //iphne6 plus
-		$(".page3_text_text").css({"fontSize":"14px","lineHeight":"18px"});
+		$(".page_67").css({"marginTop":"10%"});
+		$(".page3_text_text").css({"fontSize":"14px","lineHeight":"19px"});
 		$(".page4_renyuan").css({"fontSize":"18px","left":"8%"});
 		$(".page4_jieshao").css({fontSize:"14px","lineHeight":"19px","marginTop":"0"});
 		$(".page5_7").css({"bottom":"19%","fontSize":"20px"});
 		$(".page5_text").css({fontSize:"14px","width":"93%","lineHeight":"21px"});
 		$(".page7_text").css({"fontSize":"14px"});
 	}
-	if(bodyWidth==44 && bodyHeight==76){
-
-	}
 
 
 
 	//音乐
-	//var bgAudio = new Audio();
-	//bgAudio.loadStatus = 'unload';
-	//bgAudio.loop = true;
-	//function loadAudio(audio, url, callback) {
-	//	audio.src = url;
-	//	audio.load();
-	//	audio.addEventListener('canplay', function () {
-	//		bgAudio.loadStatus = 'loaded';
-	//		callback();
-	//	});
-	//	audio.addEventListener('loadstart', function () {
-	//		bgAudio.loadStatus = 'loading';
-	//	});
-	//}
-	//function playAudio(){
-	//	if (bgAudio.loadStatus === 'unload') {
-	//		loadAudio(bgAudio, 'media/bg.mp3', function () {
-	//			playAudio();
-	//		});
-	//		return 1;
-	//	}
-    //
-	//	bgAudio.play();
-	//}
-	//function stopAudio() {
-	//	bgAudio.pause();
-	//}
-	//bgAudio.addEventListener('playing' ,function (e) {
-	//	$('#music .music-btn').addClass('play');
-	//});
-	//bgAudio.addEventListener('pause' ,function (e) {
-	//	$('#music .music-btn').removeClass('play');
-	//});
-    //
-	//$('body').one('touchstart', function () {
-	//	playAudio();
-	//	$('#music').on('touchstart', function (e) {
-	//		if (bgAudio.paused) {
-	//			playAudio();
-	//			return 0;
-	//		}
-	//		stopAudio();
-	//		return 1;
-	//	});
-	//});
-	//window.onload = function() {
-	//	if (bgAudio.loadStatus !== 'unload') {return;}
-	//	playAudio();
-	//};
-})
+	var bgAudio = new Audio();
+	bgAudio.loadStatus = 'unload';
+	bgAudio.loop = true;
+	function loadAudio(audio, url, callback) {
+		audio.src = url;
+		audio.load();
+		audio.addEventListener('canplay', function () {
+			bgAudio.loadStatus = 'loaded';
+			callback();
+		});
+		audio.addEventListener('loadstart', function () {
+			bgAudio.loadStatus = 'loading';
+		});
+	}
+	function playAudio(){
+		if (bgAudio.loadStatus === 'unload') {
+			loadAudio(bgAudio, 'media/bg.mp3', function () {
+				playAudio();
+			});
+			return 1;
+		}
+
+		bgAudio.play();
+	}
+	function stopAudio() {
+		bgAudio.pause();
+	}
+	bgAudio.addEventListener('playing' ,function (e) {
+		$('#music .music-btn').addClass('play');
+	});
+	bgAudio.addEventListener('pause' ,function (e) {
+		$('#music .music-btn').removeClass('play');
+	});
+
+	$('body').one('touchstart', function () {
+		playAudio();
+		$('#music').on('touchstart', function (e) {
+			if (bgAudio.paused) {
+				playAudio();
+				return 0;
+			}
+			stopAudio();
+			return 1;
+		});
+	});
+	window.onload = function() {
+		if (bgAudio.loadStatus !== 'unload') {return;}
+		playAudio();
+	};
+});
 
